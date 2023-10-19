39a27c564a8e5df9a5cb21698b0ef7fe16c6d44e
sykz42 <corentin.chepeau@gmail.com>
2023-10-19 13:37:30 +0200

    Add publish to PyPI workflow

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ with open('requirements.txt') as f:
 setup(
     name='u2s_sdk',
     version='0.1.0',
+    description='Up2Share SDK for Python',
+    author='Corentin Chepeau',
+    author_email='corentin.chepeau@email.com',
     packages=find_packages(),
     install_requires=requirements,
 )
--- u2s_sdk/__init__.py
+++
@@ -1,3 +0,0 @@
-__author__ = "Corentin Chepeau"
-__email__ = "corentin.chepeau@gmail.com"
-__version__ = "0.1.0"
